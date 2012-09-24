62f479f002e36ecd351d40a3f86a02a43165a478
Ryuichi Nonaka <nonaka.ryuichi@classmethod.jp>
2012-09-24 19:27:50 +0900

    add count popup link

--- js/jquery.socialbuttons.js
+++ js/jquery.socialbuttons.js
@@ -23,10 +23,10 @@ $.fn[name_space] = function(options) {
         var $elements = this;
 
         var init_button = function ($button){
-                var $anchor       = $button.find("a");
-                var targetURL     = $anchor.attr("data-url");
+                //vars
+                var targetURL     = $button.attr("data-url");
                 var encodeURL     = encodeURI(targetURL);
-                var targetService = $anchor.attr("data-service");
+                var targetService = $button.attr("data-service");
                 
                 switch(targetService){
                         case "twitter":
@@ -35,7 +35,7 @@ $.fn[name_space] = function(options) {
                                         data      : {url : encodeURL, noncache: new Date()},
                                         dataType  : "jsonp",
                                         success   : function(result){
-                                                allot_button($anchor, result.count, targetService);
+                                                allot_button($button, result.count, targetService);
                                         }
                                 });
                                 break;
@@ -44,7 +44,7 @@ $.fn[name_space] = function(options) {
                                         url       : "http://graph.facebook.com/" + encodeURL,
                                         dataType  : "jsonp",
                                         success   : function(result){
-                                                allot_button($anchor, result.shares, targetService);
+                                                allot_button($button, result.shares, targetService);
                                         }
                                 });
                                 break;
@@ -54,7 +54,7 @@ $.fn[name_space] = function(options) {
                                         data      : {url : encodeURL},
                                         dataType  : "jsonp",
                                         success   : function(result){
-                                                allot_button($anchor, result, targetService);
+                                                allot_button($button, result, targetService);
                                         }
                                 });
                                 break;
@@ -62,35 +62,61 @@ $.fn[name_space] = function(options) {
                 }
         };
 
-        var allot_button = function($anchor, count, targetService){
+        var allot_button = function($button, count, targetService){
                 var popupURL;
-                var targetURL     = $anchor.attr("data-url");
-                var encodeURL     = encodeURI(targetURL);
-                var title         = $anchor.attr('data-title');
+                var countURL;
+                var label;
+
+                var targetURL     = $button.attr("data-url");
+                var title         = $button.attr('data-title');
                 var encodeTitle   = encodeURI(title);
 
                 switch(targetService){
                         case "twitter":
-                                popupURL = "http://twitter.com/share?text=" + encodeTitle + "&amp;url=" + encodeURL;
+                                popupURL = "http://twitter.com/share?text=" + encodeTitle + "&amp;url=" + encodeURIComponent(targetURL);
+                                countURL = "http://twitter.com/#!/search/realtime/" + encodeURIComponent(targetURL);
+                                label    = "ツイート";
                                 break;
                         case "facebook":
-                                popupURL = "http://www.facebook.com/sharer.php?u=" + encodeURL + "&amp;t=" + encodeTitle;
+                                popupURL = "http://www.facebook.com/sharer.php?u=" + encodeURIComponent(targetURL) + "&amp;t=" + encodeTitle;
+                                countURL = "http://www.facebook.com/sharer.php?u=" + encodeURIComponent(targetURL) + "&amp;t=" + encodeTitle;
+                                label    = "いいね！";
                                 break;
                         case "hatena":
-                                popupURL = "http://b.hatena.ne.jp/add?mode=confirm&amp;url=" + encodeURL + "&amp;title=" + encodeTitle;
+                                popupURL = "http://b.hatena.ne.jp/add?mode=confirm&amp;url=" + encodeURIComponent(targetURL) + "&amp;title=" + encodeTitle;
+                                countURL = "http://b.hatena.ne.jp/entry/" + encodeURI(targetURL.replace("http://", ""));
+                                label    = "";
                                 break;
                 }
 
-                $anchor.attr("href", popupURL);
-                $anchor.find('.count').html(count);
-                $anchor.click(function(){
-                        var $this = $(this);
-                        var url   = $this.attr("href");
+                $button.append(
+                        $("<a>")
+                                .addClass("icon")
+                                .attr("href", popupURL)
+                                .html(label)
+                                .click(function(){
+                                        var $this = $(this);
+                                        var url   = $this.attr("href");
 
-                        window.open(url, 'socialButtonWindow', 'width=600, height=400, menubar=no, toolbar=no, scrollbars=yes, status=no');
+                                        window.open(url, 'socialButtonWindow', 'width=600, height=400, menubar=no, toolbar=no, scrollbars=yes, status=no');
 
-                        return false;
-                });
+                                        return false;
+                                })
+                );
+                $button.append(
+                        $("<a>")
+                                .addClass("count")
+                                .attr("href", countURL)
+                                .html(count)
+                                .click(function(){
+                                        var $this = $(this);
+                                        var url   = $this.attr("href");
+
+                                        window.open(url, 'socialCountWindow', 'width=960, height=600, menubar=no, toolbar=no, scrollbars=yes, status=no');
+
+                                        return false;
+                                })
+                );
         }
 
         $elements.each(function(){
